18f43588d20da13345278e14357a23145b5f6ab8
Amit Singh <amit8829.jnnce@gmail.com>
2021-10-12 08:22:24 +0530

    Adding configuration for accounts servic

--- accounts/src/main/java/com/amit/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/amit/accounts/controller/AccountsController.java
@@ -1,9 +1,15 @@
 package com.amit.accounts.controller;
 
+import com.amit.accounts.config.AccountsServiceConfig;
 import com.amit.accounts.model.Accounts;
 import com.amit.accounts.model.Customer;
+import com.amit.accounts.model.Properties;
 import com.amit.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +20,9 @@ public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
 
+    @Autowired
+    private AccountsServiceConfig accountsConfig;
+
     @PostMapping("/accounts")
     public Accounts getAccountDetails(@RequestBody Customer customer){
         System.out.println("customer id.."+customer.getCustomerId());
@@ -22,5 +31,14 @@ public class AccountsController {
         if(accounts != null)
             return accounts;
         return null;
+    }
+
+    @GetMapping("/accounts/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+                accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
