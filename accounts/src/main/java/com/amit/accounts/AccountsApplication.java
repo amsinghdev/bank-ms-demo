fd60e87bc72b2af794f1c05234be76b978ed3783
Amit Singh <amit8829.jnnce@gmail.com>
2023-06-13 07:51:45 +0530

    Service Discovery added with spring udate

--- accounts/src/main/java/com/amit/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/amit/accounts/AccountsApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 @RefreshScope
 @EnableJpaRepositories("com.amit.accounts.repository")
 @EntityScan("com.amit.accounts.model")
